95e81a3238faef567cd06651047646113f016fe8
Pierre Wolinski <pierre.wolinski@inria.fr>
2023-02-23 18:12:09 +0100

    added setup.py and a prototype

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='gaussian-preact',
+    version='0.1.0',
+    author='Pierre Wolinski',
+    author_email='pierre.wolinski@normalesup.org',
+    packages=['gaussian_preact'],
+    url='https://github.com/p-wol/gaussian-preact',
+    license='LICENSE',
+    description='Computations related to the paper "Gaussian Pre-Activations in Neural Networks: Myth or Reality?"',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "pytorch >= 1.13.1",
+    ],
+)

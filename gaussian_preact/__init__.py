5780e0b1f933d4fc23a05b54f13022a4a96d577d
Pierre Wolinski <pierre.wolinski@normalesup.org>
2024-06-26 21:26:56 +0200

    ActivationFunction: Parameter -> buffer; pickle -> torch-pickle

--- gaussian_preact/__init__.py
+++ gaussian_preact/__init__.py
@@ -1,3 +1,3 @@
 from .integration import ParameterizedFunction, Integrand
 from .optimization import find_density, find_activation, surv_Gaussian_abs, build_surv_kernel_Weib
-from .activation import ActivationFunction, ActivationFunctionPosTraining, ActivationFunctionPos
+from .activation import ActivationFunctionTraining, ActivationFunction, ActivationFunctionPosTraining, ActivationFunctionPos
--- gaussian_preact/activation.py
+++ gaussian_preact/activation.py
@@ -2,7 +2,14 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-class ActivationFunction(torch.nn.Module):
+def act_function(x, alpha, a, b, c, d, f, g):
+    """
+    f(x) = a * tanh(b * x) * ((|x| + c)^alpha + d) * (log(|x| + f) + g)
+    """
+    return a * torch.tanh(b * x) * (torch.pow(torch.abs(x) + c, alpha) + d) * \
+                (torch.log(torch.abs(x) + f) + g)
+
+class ActivationFunctionTraining(torch.nn.Module):
     """
     Function representing the activation function, built in such a way it is easy to compute
     and backpropagate for pytorch.
@@ -10,7 +17,7 @@ class ActivationFunction(torch.nn.Module):
     f(x) = a * tanh(b * x) * ((|x| + c)^alpha + d) * (log(|x| + f) + g)
     """
     def __init__(self, theta, a, b, c, d, f, g):
-        super(ActivationFunction, self).__init__()
+        super(ActivationFunctionTraining, self).__init__()
         dtype = torch.get_default_dtype()
 
         alpha = 1 - 2/theta
@@ -26,12 +33,10 @@ class ActivationFunction(torch.nn.Module):
         
     def forward(self, x):
         if self.train:
-            return self.a * torch.tanh(self.b * x) * (torch.pow(torch.abs(x) + self.c, self.alpha) + self.d) * \
-                (torch.log(torch.abs(x) + self.f) + self.g)
+            return act_function(x, self.alpha, self.a, self.b, self.c, self.d, self.f, self.g)
         else:
             with torch.no_grad():
-                return self.a * torch.tanh(self.b * x) * (torch.pow(torch.abs(x) + self.c, self.alpha) + self.d) * \
-                    (torch.log(torch.abs(x) + self.f) + self.g)
+                return act_function(x, self.alpha, self.a, self.b, self.c, self.d, self.f, self.g)
 
     def forward_ng(self, x):
         with torch.no_grad():
@@ -106,6 +111,43 @@ class ActivationFunction(torch.nn.Module):
                 x = x.to(device = self.a.device)
 
             return self.derivative2(x)
+
+class ActivationFunction(torch.nn.Module):
+    """
+    Function representing the activation function, built in such a way it is easy to compute
+    and backpropagate for pytorch.
+
+    f(x) = a * tanh(b * x) * ((|x| + c)^alpha + d) * (log(|x| + f) + g)
+    """
+    def __init__(self, act_function):
+        super(ActivationFunction, self).__init__()
+        dtype = torch.get_default_dtype()
+
+        self.register_buffer('alpha', act_function.alpha.clone().detach())
+        
+        self.register_buffer('a', act_function.a.clone().detach())
+        self.register_buffer('b', act_function.b.clone().detach())
+        self.register_buffer('c', act_function.c.clone().detach())
+        self.register_buffer('d', act_function.d.clone().detach())
+        
+        self.register_buffer('f', act_function.f.clone().detach())
+        self.register_buffer('g', act_function.g.clone().detach())
+        
+    def forward(self, x):
+        if self.train:
+            return act_function(x, self.alpha, self.a, self.b, self.c, self.d, self.f, self.g)
+        else:
+            with torch.no_grad():
+                return act_function(x, self.alpha, self.a, self.b, self.c, self.d, self.f, self.g)
+
+    def forward_ng(self, x):
+        with torch.no_grad():
+            if not torch.is_tensor(x):
+                x = torch.tensor(x, device = self.a.device)
+            elif x.device != self.a.device:
+                x = x.to(device = self.a.device)
+
+            return self.forward(x)
 
 def act_function_pos(x, alpha, a, b, c, d, f, g, h):
     """

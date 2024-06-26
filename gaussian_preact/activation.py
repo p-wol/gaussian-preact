0aa143d49abeb73902de7cab2d39b6a4d738eb0f
Pierre Wolinski <pierre.wolinski@inria.fr>
2024-06-26 17:01:14 +0200

    up

--- gaussian_preact/activation.py
+++ gaussian_preact/activation.py
@@ -106,3 +106,79 @@ class ActivationFunction(torch.nn.Module):
                 x = x.to(device = self.a.device)
 
             return self.derivative2(x)
+
+def act_function_pos(x, alpha, a, b, c, d, f, g, h):
+    """
+    f(x) = a * sigmoid(b * x + h) * ((softplus(x) + c)^alpha + d) * (log(softplus(x) + f) + g)
+    """
+    return a * torch.sigmoid(b * x + h) * (torch.pow(torch.nn.functional.softplus(x) + c, alpha) + d) * \
+        (torch.log(torch.nn.functional.softplus(x) + f) + g)
+
+class ActivationFunctionPosTraining(torch.nn.Module):
+    """
+    Function representing the activation function, built in such a way it is easy to compute
+    and backpropagate for pytorch.
+
+    f(x) = a * sigmoid(b * x + h) * ((softplus(x) + c)^alpha + d) * (log(softplus(x) + f) + g)
+    """
+    def __init__(self, theta, a, b, c, d, f, g, h):
+        super(ActivationFunctionPosTraining, self).__init__()
+        dtype = torch.get_default_dtype()
+
+        alpha = 1 - 2/theta
+        
+        self.register_buffer('alpha', torch.tensor(alpha))
+        self.a = nn.Parameter(torch.tensor(a, dtype = dtype))
+        self.b = nn.Parameter(torch.tensor(b, dtype = dtype))
+        self.h = nn.Parameter(torch.tensor(h, dtype = dtype))
+        self.c = nn.Parameter(torch.tensor(c, dtype = dtype))
+        self.d = nn.Parameter(torch.tensor(d, dtype = dtype))
+        
+        self.f = nn.Parameter(torch.tensor(f, dtype = dtype))
+        self.g = nn.Parameter(torch.tensor(g, dtype = dtype))
+        
+    def forward(self, x):
+        return act_function_pos(x, self.alpha, self.a, self.b, self.c, self.d, self.f, self.g, self.h)
+
+    def forward_ng(self, x):
+        with torch.no_grad():
+            if not torch.is_tensor(x):
+                x = torch.tensor(x, device = self.a.device)
+            elif x.device != self.a.device:
+                x = x.to(device = self.a.device)
+
+            return self.forward(x)
+
+class ActivationFunctionPos(torch.nn.Module):
+    """
+    Function representing the activation function, built in such a way it is easy to compute
+    and backpropagate for pytorch.
+
+    f(x) = a * sigmoid(b * x + h) * ((softplus(x) + c)^alpha + d) * (log(softplus(x) + f) + g)
+    """
+    def __init__(self, act_function):
+        super(ActivationFunctionPos, self).__init__()
+        dtype = torch.get_default_dtype()
+        
+        self.register_buffer('alpha', act_function.alpha.clone().detach())
+        
+        self.register_buffer('a', act_function.a.clone().detach())
+        self.register_buffer('b', act_function.b.clone().detach())
+        self.register_buffer('h', act_function.h.clone().detach())
+        self.register_buffer('c', act_function.c.clone().detach())
+        self.register_buffer('d', act_function.d.clone().detach())
+        
+        self.register_buffer('f', act_function.f.clone().detach())
+        self.register_buffer('g', act_function.g.clone().detach())
+        
+    def forward(self, x):
+        return act_function_pos(x, self.alpha, self.a, self.b, self.c, self.d, self.f, self.g, self.h)
+
+    def forward_ng(self, x):
+        with torch.no_grad():
+            if not torch.is_tensor(x):
+                x = torch.tensor(x, device = self.a.device)
+            elif x.device != self.a.device:
+                x = x.to(device = self.a.device)
+
+            return self.forward(x)

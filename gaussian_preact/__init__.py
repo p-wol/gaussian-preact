65b4c215058872b3c530329ff895911a0aff3962
Pierre Wolinski <pierre.wolinski@inria.fr>
2024-06-27 13:47:31 +0200

    added notebook to reproduce Fig. 11 (influence of dependence)

--- gaussian_preact/__init__.py
+++ gaussian_preact/__init__.py
@@ -1,3 +1,4 @@
 from .integration import ParameterizedFunction, Integrand
 from .optimization import find_density, find_activation, surv_Gaussian_abs, build_surv_kernel_Weib
 from .activation import ActivationFunctionTraining, ActivationFunction, ActivationFunctionPosTraining, ActivationFunctionPos
+from .samplers import sampler_normal, build_sampler_weibull
